e09de71514a4c17cbdcb82c3b5050279519eb0b1
francocuervoo <77750393+francocuervoo@users.noreply.github.com>
2021-08-06 11:30:42 -0300

    Add files via upload

---
+++ desafio.js
@@ -0,0 +1,158 @@
+/*Importo el módulo fs*/ const fs = require("fs");
+/*Importo express */ const express = require("express");
+/*Aplicación servidor*/ const app = express();
+
+//Productos
+let p1 = {
+  title: "Heladera",
+  price: 80000,
+};
+
+let p2 = {
+  title: "Lavarropas",
+  price: 60000,
+};
+
+//Clase Contenedor
+class Contenedor {
+  constructor(fileName) {
+    this.fileName = fileName;
+    this.id = 0;
+    this.data = [];
+  }
+  //Métodos
+  async save(objeto) {
+    await this.getAll();
+    this.id++;
+    this.data.push({
+      id: this.id,
+      product: objeto,
+    });
+    try {
+      await fs.promises.writeFile(
+        this.fileName,
+        JSON.stringify(this.data, null, 2)
+      );
+      //Devuelvo los id asignados
+      return console.log("El id del objeto ingresado es", this.id);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getById(id) {
+    await this.getAll();
+    try {
+      await fs.promises.writeFile(
+        this.fileName,
+        JSON.stringify(this.data, null, 2)
+      );
+      //Busco el objeto con ese id
+      const objetoId = this.data.find((dat) => dat.id === id);
+      if (objetoId) {
+        return console.log("El objeto con el id", id, "es", objetoId);
+      }
+    } catch (error) {
+      return null;
+    }
+  }
+
+  async getAll() {
+    //Manejo error con try catch
+    try {
+      const data = await fs.promises.readFile(this.fileName, "utf-8");
+      if (data) {
+        //Paso data a un objeto
+        this.data = JSON.parse(data);
+        this.data.map((producto) => {
+          //Obtengo cual es el id máximo del archivo
+          if (this.id < producto.id) this.id = producto.id;
+        });
+        return console.log(JSON.parse(data));
+      }
+    } catch (error) {
+      //Si hay un error que no retorne nada. Que siga.
+      return;
+    }
+  }
+
+  async deleteById(id) {
+    try {
+      const data = await fs.promises.readFile(this.fileName, "utf-8");
+      if (data) {
+        //Paso data a un objeto
+        this.data = JSON.parse(data);
+        //Nuevo array sin el objeto con el id pasado
+        const objetoSinId = this.data.filter((dat) => dat.id != id);
+        //Guardo el nuevo array en el archivo
+        await fs.promises.writeFile(
+          this.fileName,
+          JSON.stringify(objetoSinId, null, 2)
+        );
+        console.log(`El producto con el id ${id} fue eliminado.`);
+      }
+    } catch (error) {
+      //Si hay un error que no retorne nada. Que siga.
+      return;
+    }
+  }
+
+  async deleteAll() {
+    this.data = [];
+    try {
+      await fs.promises.writeFile(
+        this.fileName,
+        JSON.stringify(this.data, null, 2)
+      );
+      return console.log("Se borraron todos los objetos del archivo");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+const objetoContenedor = new Contenedor("productos.txt");
+
+async function allFunctionsContenedor() {
+  // await objetoContenedor.save(p1);
+  // await objetoContenedor.save(p2);
+  // await objetoContenedor.getAll();
+  // await objetoContenedor.getById(3);
+  // await objetoContenedor.deleteById(3);
+  // await objetoContenedor.deleteAll();
+}
+allFunctionsContenedor();
+
+async function readFile() {
+  await objetoContenedor.getAll();
+}
+
+app.get("/", (req, res) => {
+  res.send("<h1 style='color: #4B4B4B'>Desafío Clase 3</h1>");
+});
+
+app.get("/productos", (req, res) => {
+  //Acá quiero que lea el archivo y me devuelve el objeto vacío
+  res.send(readFile());
+});
+
+app.get("/productosRandom", (req, res) => {
+  //ACá debería devolver uno de esos productos de forma aleatoria (devuelve error)
+  res.send(Math.random(readFile()));
+});
+
+
+const PORT = 8080;
+
+const server = app.listen(PORT, () => {
+  console.log(`Servidor express corriendo en el puerto ${PORT}`);
+});
+
+server.on("error", (error) => console.log(`Error en servidor ${error}`));
+
+
+
+
+//npm run express
+//ver de neuvo lo de glitch
+// minuto 1.47.41
